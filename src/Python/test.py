7fd28c30562710af1c713b78f5916e2a0fc24c31
Eduardo Anglada <eduardo.anglada@intersystems.com>
2022-03-29 13:01:33 +0200

    updates

--- src/Python/test.py
+++ src/Python/test.py
@@ -1,12 +1,18 @@
 import sys
 import os
 
-# Comprobar que el servicio "callin" esté activo
+# Si se produce un error de autenticación ...
+# ... comprobar que el servicio "callin" esté activo
 os.environ["IRISUSERNAME"]="SuperUser"
 os.environ["IRISPASSWORD"]="SYS"
 os.environ["IRISNAMESPACE"]="USER"
 
+#Windows
 sys.path += ['C:\InterSystems\IRIS20212\mgr\python',"C:\InterSystems\IRIS20212\lib\python"]
+
+#Linux container
+#sys.path += ['/usr/irissys/mgr/python',"/usr/irissys/lib/python"]
+
 
 import iris
 
