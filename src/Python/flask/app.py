8ef2b6496dcce3b641374b143be0aed7a6867029
Eduardo Anglada <eduardo.anglada@intersystems.com>
2022-03-29 16:19:46 +0200

    more updates

--- src/Python/flask/app.py
+++ src/Python/flask/app.py
@@ -7,8 +7,19 @@ from matplotlib.backends.backend_svg import FigureCanvasSVG
 
 from matplotlib.figure import Figure
 
+import logging
+
 app = Flask(__name__) 
 app.secret_key = "abc222"
+
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+ 
+@app.route('/blogs')
+def blog():
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+    return f"Welcome to the Blog"
+
 
 # ----------------------------------------------------------------
 ### CRUD FOR TITANIC_TABLE.PASSENGER
