b7c12c346bcdf15d62cc69d0f5209dab33c7004c
Eduardo Anglada <eduardo.anglada@intersystems.com>
2022-03-29 12:13:09 +0200

    another example

---
+++ src/Python/test.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# Comprobar que el servicio "callin esté activo"
+os.environ["IRISUSERNAME"]="SuperUser"
+os.environ["IRISPASSWORD"]="SYS"
+os.environ["IRISNAMESPACE"]="USER"
+
+sys.path += ['C:\InterSystems\IRIS20212\mgr\python',"C:\InterSystems\IRIS20212\lib\python"]
+
+import iris
+
+print (iris)
+
+
+rs = iris.sql.exec("SELECT TOP 10 * From Data.Titanic")
+
+for row in rs:
+    print(row)
